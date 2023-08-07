fccdd82597309dd86f95cba46e63be831d916c92
Keaton Sentak <ksentak01@gmail.com>
2023-08-07 19:34:56 -0400

    begin working on general structure

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
 import axios from 'axios';
 import 'dotenv/config';
 
+const apiKey = process.env.ODDS_API_KEY;
+
+// Add JSDoc
+const getInSeasonSports = async () => {
+  const url = `https://api.the-odds-api.com/v4/sports?apiKey=${apiKey}`;
+
+  try {
+    const res = await axios.get(url);
+    console.log(res.data);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Add JSDoc
+const getOdds = async () => {
+  const apiKey = process.env.ODDS_API_KEY;
+  const url = `https://api.the-odds-api.com/v4/sports/americanfootball_nfl/odds/?regions=us&oddsFormat=american&apiKey=${apiKey}`;
+
+  try {
+    const res = await axios.get(url);
+    console.log(res);
+    console.log(res.data);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Function to format odds
+const formatMsg = (msg) => {};
+
+// Add JSDoc
 const sendDiscordMsg = async (msg) => {
   const url = process.env.DISCORD_BOT_URL;
 
@@ -17,3 +49,5 @@ const sendDiscordMsg = async (msg) => {
     console.error(err);
   }
 };
+
+getOdds();
